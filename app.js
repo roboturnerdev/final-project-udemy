a3795f648cd698fca70d45fe2c22c70a19be5f43
Robert Turner <roboturnerdev@gmail.com>
2021-10-30 22:53:02 -0400

    initial commit for express and ejs installed and initialized. server and basic route are connected, project ready to begin.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert("this is crazy");
+// **********
+// YelpCamp!
+// **********
+
+// dependencies
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// config
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware
+
+
+
+// routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// start server on port 3000 cause why not
+app.listen(3000, () => {
+    console.log("server port 3000");
+});
