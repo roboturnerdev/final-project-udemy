b797d5f06057dfa70c6be868bb1f93c25cea7951
Robert Turner <roboturnerdev@gmail.com>
2021-10-31 01:19:47 -0400

    method override brought in and used for the put requests and express. edit function in and working.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// **********
+////////////////
 // YelpCamp!
-// **********
+////////////////
 
 // dependencies
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 // connect mongoose and name DB for yelp-camp
@@ -34,7 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // need to tell express to parse body in requests
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 
 // routes
 app.get('/', (req, res) => {
@@ -51,16 +52,50 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
+// first post request to create a new campground and add to db
 app.post('/campgrounds/', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+
+    // redirect to the show page for the added campground
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
-// place routes above /:id routes so they dont get it before
+// make sure to order these routes so that they don't
+// prevent later ones from occuring
+
+
 app.get('/campgrounds/:id', async (req, res) => {
+    // see if the request has a good campground
     const campground = await Campground.findById(req.params.id);
+
+    // render show site of this campground
     res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    // see if the request could find this campground
+    const campground = await Campground.findById(req.params.id);
+
+    // render edit site for this campground
+    res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    // res.send("IT WORKED!");
+    // we know adding the method-override worked for the put request
+    
+    // destructure the id from the request
+    const { id } = req.params;
+    
+    // we referenced this campground in new and edit
+    // as an array with properties. because of that
+    // we can use the spread operator for the params
+    // we want to send in (title, location, etc)
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    
+    // show page of the campground we just created
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 
