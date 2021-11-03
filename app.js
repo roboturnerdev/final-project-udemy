5daf2abc4db56ee880259a5d5c95646c7c5ebc5e
Robert Turner <roboturnerdev@gmail.com>
2021-11-03 19:35:12 -0400

    Refactor campground routes with express router.

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
+const campgrounds = require('./routes/campgrounds');
+
 // connect mongoose and name DB for yelp-camp
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -44,22 +46,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-const validateCampground = (req, res, next) => {
-    // new more explicit error handing with
-    // Joi validation
-    // helps even if the client-side validation doesnt stop
-    // the problem
-    // i.e., postman sending post requests with missing stuff
-    // skips the form submit preventative error handling
-   
-     const { error } = campgroundSchema.validate(req.body);
-     if(error) {
-         const msg = error.details.map(el => el.message).join(',');
-         throw new ExpressError(msg, 400);
-     } else {
-         next();
-     }
-};
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
@@ -71,73 +57,14 @@ const validateReview = (req, res, next) => {
     }
 };
 
+app.use('/campgrounds', campgrounds);
+
 // routes
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-// all campgrounds page
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}));
 
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
-
-// first post request to create a new campground and add to db
-app.post('/campgrounds/', validateCampground, catchAsync(async (req, res, next) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid campground data dewd', 400);
-    
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    // redirect to the show page for the added campground
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-// make sure to order these routes so that they don't
-// prevent later ones from occuring
-
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    // see if the request has a good campground
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    // render show site of this campground
-    res.render('campgrounds/show', { campground });
-}));
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    // see if the request could find this campground
-    const campground = await Campground.findById(req.params.id);
-
-    // render edit site for this campground
-    res.render('campgrounds/edit', { campground });
-}));
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    // res.send("IT WORKED!");
-    // we know adding the method-override worked for the put request
-    
-    // destructure the id from the request
-    const { id } = req.params;
-    
-    // we referenced this campground in new and edit
-    // as an array with properties. because of that
-    // we can use the spread operator for the params
-    // we want to send in (title, location, etc)
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-    
-    // show page of the campground we just created
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-// delete route
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
 
 // initial route for post request
 // submit review to db and add review to campground
@@ -178,7 +105,7 @@ app.listen(3000, () => {
 });
 
 
-// Section 49
+// Section 49 - current branch/section
 // Robert Turner, 2021
 // @robo_turner
 // roboturnerdev@gmail.com
---
+++ routes/campgrounds.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const { campgroundSchema } = require('../schemas.js');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+
+const validateCampground = (req, res, next) => {
+    // new more explicit error handing with
+    // Joi validation
+    // helps even if the client-side validation doesnt stop
+    // the problem
+    // i.e., postman sending post requests with missing stuff
+    // skips the form submit preventative error handling
+   
+     const { error } = campgroundSchema.validate(req.body);
+     if(error) {
+         const msg = error.details.map(el => el.message).join(',');
+         throw new ExpressError(msg, 400);
+     } else {
+         next();
+     }
+};
+
+// add routes to router instead
+// all campgrounds page
+router.get('/', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}));
+
+router.get('/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+// first post request to create a new campground and add to db
+router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid campground data dewd', 400);
+    
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    // redirect to the show page for the added campground
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+// make sure to order these routes so that they don't
+// prevent later ones from occuring
+
+router.get('/:id', catchAsync(async (req, res) => {
+    // see if the request has a good campground
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    // render show site of this campground
+    res.render('campgrounds/show', { campground });
+}));
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    // see if the request could find this campground
+    const campground = await Campground.findById(req.params.id);
+
+    // render edit site for this campground
+    res.render('campgrounds/edit', { campground });
+}));
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+    // res.send("IT WORKED!");
+    // we know adding the method-override worked for the put request
+    
+    // destructure the id from the request
+    const { id } = req.params;
+    
+    // we referenced this campground in new and edit
+    // as an array with properties. because of that
+    // we can use the spread operator for the params
+    // we want to send in (title, location, etc)
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    
+    // show page of the campground we just created
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+// delete route
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+}));
+
+module.exports = router;
