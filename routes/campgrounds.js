03fa7e0affd6310fa8081022398536c5e6ed124c
Robert Turner <roboturnerdev@gmail.com>
2021-11-06 18:43:37 -0400

    remove gitignore files

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,8 +4,10 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const Campground = require('../models/campground');
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const multer  = require('multer');
+const { storage } = require('../cloudinary/'); // node automatically looks for index.js in a folder
+const upload = multer(storage);
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
