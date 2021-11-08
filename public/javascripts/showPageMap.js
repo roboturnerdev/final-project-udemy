870f127b36eac160653e8bb2566e1d549e53ea71
Robert Turner <roboturnerdev@gmail.com>
2021-11-08 16:57:59 -0500

    geodata used to show a map, center map on location, and a marker

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 9 // starting zoom
+});
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
