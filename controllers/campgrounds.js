62660a931a695831e8d40408fc5159a8d625f062
Robert Turner <roboturnerdev@gmail.com>
2021-11-08 16:36:46 -0500

    geojson geometry property is added to campground model and working

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -21,17 +21,15 @@ module.exports.createCampground = async (req, res, next) => {
         query: req.body.campground.location,
         limit: 1
     }).send();
-    res.send(geoData.body.features[0].geometry.coordinates);
 
-    // const campground = new Campground(req.body.campground);
-    // // map over the files from multer and get just path and filename
-    // // assign them to the campground image properties
-    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
-    // campground.author = req.user._id;
-    // await campground.save();
-    // console.log(campground);
-    // req.flash('success', 'Successfully made a new campground!');
-    // res.redirect(`/campgrounds/${campground._id}`);
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.author = req.user._id;
+    await campground.save();
+    console.log(campground);
+    req.flash('success', 'Successfully made a new campground!');
+    res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.showCampground = async (req, res) => {
