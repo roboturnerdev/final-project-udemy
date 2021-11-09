6b87ad8b023b21cf1dcd097c42bb775b5055c748
Robert Turner <roboturnerdev@gmail.com>
2021-11-09 17:17:52 -0500

    final commit section 55. map is in. looked at examples of markers, popups, and custom map styles in docs.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  style: 'mapbox://styles/mapbox/dark-v10', // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(  // popup is on the marker
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`  // very basic
+        )
+    )
     .addTo(map);
