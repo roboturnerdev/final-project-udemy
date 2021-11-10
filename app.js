de7a13c0147b8d62995c8d455c97e582c1c3a006
Robert Turner <roboturnerdev@gmail.com>
2021-11-10 02:22:46 -0500

    add start script

--- app.js
+++ app.js
@@ -214,9 +214,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-    // get server listening 3000
-    console.log("Server Port: 3000");
+const port = process.env.PORT || 3000;
+app.listen((port,) () => {
+    console.log(`server port: ${port}`);
     // antithesis to the 'no-spin zone'
     // we are spinning
     console.log("estamos girando");
